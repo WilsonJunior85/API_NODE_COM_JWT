4a2083897be171c815ae626af0db1b5a6be8e3d3
WILSON JUNIOR <wilson.pereira@serede.com.br>
2025-11-26 18:51:36 -0300

    Criando rotas privadas e o auth

---
+++ middlewares/auth.js
@@ -0,0 +1,21 @@
+import jwt from "jsonwebtoken";
+
+const JWT_SECRET = process.env.JWT_SECRET;
+
+const auth = (req, res, next) => {
+  const token = req.headers.authorization;
+
+  if (!token) {
+    return res.status(401).json({ message: "Acesso negado!" });
+  }
+  try {
+    const decodificado = jwt.verify(token.replace("Bearer ", ""), JWT_SECRET);
+    req.usrId = decodificado.id;
+  } catch (err) {
+    return res.status(401).json({ message: "Token Inválido" });
+  }
+
+  next();
+};
+
+export default auth;
