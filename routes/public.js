13e6477fc898b50386d8522a8886e5df8ca746b8
WILSON JUNIOR <wilson.pereira@serede.com.br>
2025-11-26 11:40:38 -0300

    Criando rotas públicas e Token JWT para autenticação

--- routes/public.js
+++ routes/public.js
@@ -1,9 +1,12 @@
 import express from "express";
 import bcrypt from "bcrypt";
 import { PrismaClient } from "@prisma/client";
+import jwt from "jsonwebtoken";
 
 const prisma = new PrismaClient();
 const router = express.Router();
+
+const JWT_SECRET = process.env.JWT_SECRET;
 
 // Cadastro
 router.post("/cadastro", async (req, res) => {
@@ -37,12 +40,16 @@ router.post("/login", async (req, res) => {
     if (!user) {
       return res.status(404).json({ message: "Usuário não encontrado" });
     }
+    // Compara o que o usuário digitou
     const isMatch = await bcrypt.compare(userInfo.password, user.password);
     if (!isMatch) {
       return res.status(400).json({ message: "Senha Inválida" });
     }
 
-    res.status(200).json(user);
+    //Gerar p Token JWT
+    const token = jwt.sign({ id: user.id }, JWT_SECRET, { expiresIn: "1m" });
+
+    res.status(200).json(token);
   } catch (err) {
     res.status(500).json({ message: "Erro no Servidor, tente novamente" });
   }
